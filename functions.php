4f14a3b764b9a069a3d0768c5c4598abe623b6c0
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-03-15 13:23:11 -0500

    enqueue google fonts

--- functions.php
+++ functions.php
@@ -102,6 +102,8 @@ add_action( 'widgets_init', 'sanctuary_widgets_init' );
 function sanctuary_scripts() {
 	wp_enqueue_style( 'sanctuary-style', get_stylesheet_uri() );
 
+	wp_enqueue_style('sanctuary-google-fonts', 'http://fonts.googleapis.com/css?family=Arvo:400,700|Open+Sans:700,400|Open+Sans+Condensed:300' );
+
 	wp_enqueue_script( 'sanctuary-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'sanctuary-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
